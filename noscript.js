a66facbc2d8418c48ce43e96e88abb6da055ddf8
carossst <159724698+carossst@users.noreply.github.com>
2026-01-06 10:18:54 -1000

    Delete noscript.js

--- noscript.js
+++
@@ -1,8 +0,0 @@
-// js/noscript.js
-
-document.addEventListener('DOMContentLoaded', function () {
-    const btn = document.getElementById('reload-btn');
-    if (btn) {
-        btn.addEventListener('click', () => window.location.reload());
-    }
-});
